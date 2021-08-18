ec548425d786b50acae9160c2c3af9ccb0093833
eunmoong <eunmoong@gmail.com>
2021-08-19 02:02:14 +0900

    css수정

--- home/forms.py
+++ home/forms.py
@@ -4,9 +4,9 @@ from .models import *
 # Create your models here.
 class PostForm(forms.ModelForm):
     title = forms.CharField(label='제목', max_length=50, 
-        widget=forms.Textarea(attrs={'rows':'1', 'cols': '80'}))
+        widget=forms.Textarea(attrs={'rows':'1', 'cols': '70'}))
     body = forms.CharField(label='내용', max_length=1000, 
-        widget=forms.Textarea(attrs={'rows':'15', 'cols': '80'}))
+        widget=forms.Textarea(attrs={'rows':'15', 'cols': '70'}))
     class Meta:
         model = Post
         fields =['title','body','image']
