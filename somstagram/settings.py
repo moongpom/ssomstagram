708ea3c622fd8a086d45184e539538f6d084c31a
alexiskim66 <ehpark10t@gmail.com>
2021-08-20 22:05:47 +0900

    add files for deploying to heroku

--- somstagram/settings.py
+++ somstagram/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import json
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,6 +52,9 @@ INSTALLED_APPS = [
 
     # provider 구글. 페북.카톡.깃헙
     'allauth.socialaccount.providers.google',
+
+    #s3 업로드
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -169,3 +174,27 @@ REST_FRAMEWORK = {
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+#환경변수설정
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_env_variable(key):
+    try:
+        return secrets[key]
+    except KeyError:
+        error_msg = f"Set the {key} environment variable"
+        raise ImproperlyConfigured(error_msg)
+
+AWS_ACCESS_KEY_ID = get_env_variable("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = get_env_variable("AWS_SECRET_ACCESS_KEY")
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_STORAGE_BUCKET_NAME = 'somstagram'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'ap-northeast-2'
+AWS_S3_CUSTOM_DOMAIN = 'somstagram.s3.ap-northeast-2.amazonaws.com'
+AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
